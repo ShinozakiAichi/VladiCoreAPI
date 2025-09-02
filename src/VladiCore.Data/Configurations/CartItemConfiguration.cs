73369c22fc1b7f3fe4a8c0873e7353d9a0daeebc
Shinozaki Aichi <155703392+ShinozakiAichi@users.noreply.github.com>
2025-09-02 14:43:29 +0300

    Add schema-aware configurations and partial indexes

--- src/VladiCore.Data/Configurations/CartItemConfiguration.cs
+++ src/VladiCore.Data/Configurations/CartItemConfiguration.cs
@@ -8,14 +8,34 @@ public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
 {
     public void Configure(EntityTypeBuilder<CartItem> builder)
     {
-        builder.ToTable("cart_items");
+        builder.ToTable("cart_items", "sales");
         builder.HasKey(i => i.Id);
         builder.Property(i => i.UnitPrice).HasColumnType("numeric(12,2)");
         builder.Property(i => i.CreatedAt).HasDefaultValueSql("now()");
-        builder.HasCheckConstraint("CK_cartitem_product_variant", "(\"ProductId\" IS NOT NULL)::int + (\"VariantId\" IS NOT NULL)::int = 1");
+
+        builder.HasCheckConstraint("ck_cart_items_xor", "(product_id IS NOT NULL) <> (variant_id IS NOT NULL)");
+
         builder.HasOne(i => i.Cart)
                .WithMany(c => c.Items)
-               .HasForeignKey(i => i.CartId);
-        builder.HasIndex(i => new { i.CartId, i.ProductId, i.VariantId }).IsUnique();
+               .HasForeignKey(i => i.CartId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(i => i.Product)
+               .WithMany()
+               .HasForeignKey(i => i.ProductId)
+               .OnDelete(DeleteBehavior.SetNull);
+
+        builder.HasOne(i => i.Variant)
+               .WithMany()
+               .HasForeignKey(i => i.VariantId)
+               .OnDelete(DeleteBehavior.SetNull);
+
+        builder.HasIndex(i => new { i.CartId, i.ProductId })
+               .IsUnique()
+               .HasFilter("product_id IS NOT NULL");
+
+        builder.HasIndex(i => new { i.CartId, i.VariantId })
+               .IsUnique()
+               .HasFilter("variant_id IS NOT NULL");
     }
 }
--- src/VladiCore.Data/Configurations/UserAddressConfiguration.cs
+++ src/VladiCore.Data/Configurations/UserAddressConfiguration.cs
@@ -8,12 +8,18 @@ public class UserAddressConfiguration : IEntityTypeConfiguration<UserAddress>
 {
     public void Configure(EntityTypeBuilder<UserAddress> builder)
     {
-        builder.ToTable("user_addresses");
+        builder.ToTable("user_addresses", "core");
         builder.HasKey(a => a.Id);
         builder.Property(a => a.CreatedAt).HasDefaultValueSql("now()");
         builder.Property(a => a.UpdatedAt).HasDefaultValueSql("now()");
+
         builder.HasOne(a => a.User)
                .WithMany(u => u.Addresses)
-               .HasForeignKey(a => a.UserId);
+               .HasForeignKey(a => a.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(a => a.UserId)
+               .IsUnique()
+               .HasFilter("is_default");
     }
 }
